16ad67f2ef61a8ed244a2269385797552eb7a820
Devacrom <platarotimarco@gmail.com>
2025-05-11 10:56:13 +0200

    refactor(scripts): update scripts

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,12 @@ FROM golang:1.24-alpine
 
 WORKDIR /mnt
 
+# Install necessary build tools
+RUN apk add --no-cache make git
+
+# Create bin directory
+RUN mkdir -p /mnt/bin
+
 EXPOSE 9090
 
 ENTRYPOINT ["/bin/sh"]
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Ensure bin directory exists
+mkdir -p bin
+
 echo "Building Go binary from ./cmd/main.go..."
-go build -o bin/server ./cmd/main.go
+go build -o bin/server/subitocart ./cmd/main.go
 echo "Build completed. Binary located at ./bin/server"
